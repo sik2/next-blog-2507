5f33360450ed1c379e8cf2139642fab69c28b961
sik2 <bickwang123@gmail.com>
2025-07-11 15:55:14 +0900

    15

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,24 +1,47 @@
 "use client";
 
+import { supabase } from "@/app/lib/supabase";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 function PostDetail() {
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   const params = useParams();
   const { id } = params;
 
-  const [post, setPost] = useState({});
+  const fetchData = async () => {
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    setPost(data);
+  };
+
+  const fetchComments = async () => {
+    const { data, error } = await supabase.from("comments").select("*");
+    setComments(data);
+  };
+
   useEffect(() => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((res) => setPost(res));
+    fetchData();
+    fetchComments();
   }, []);
 
   return (
     <>
       <h1>{id}번</h1>
       <div className="text-2xl">{post.title}</div>
-      <p>{post.body}</p>
+      <p>{post.contents}</p>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id} className="text-xs underline">
+            {comment.contents}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
