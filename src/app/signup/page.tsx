26ad2c084bfcec0c26e783c5ab8582f0239b87f2
sik2 <bickwang123@gmail.com>
2025-07-15 09:37:36 +0900

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <>
-      <form>
-        <input type="email" name="email" placeholder="EMAIL" />
-        <input type="password" name="password" placeholder="PASSWORD" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="EMAIL"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="PASSWORD"
+        />
         <button type="submit">회원가입</button>
       </form>
     </>
