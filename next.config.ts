f8e9e0fffad7e4cee849b605a643731daa322306
sik2 <bickwang123@gmail.com>
2025-07-15 15:48:44 +0900

    lint off

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 빌드 시 ESLint 체크를 건너뛰기
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
