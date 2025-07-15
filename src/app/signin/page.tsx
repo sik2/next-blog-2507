69aa130a915472313e368f7a30f1974653a7c1f5
sik2 <bickwang123@gmail.com>
2025-07-15 10:48:28 +0900

    login

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,12 +1,47 @@
+"use client";
+
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 function SignIn() {
-  // supabase.auth.signInWithPassword({ email, password });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+      console.log(error);
+    } else {
+      alert("로그인 성공");
+      router.push("/");
+    }
+  };
 
   return (
     <>
       <h1>로그인</h1>
-      <form>
-        <input type="email" name="email" placeholder="EMAIL" />
-        <input type="password" name="password" placeholder="PASSWORD" />
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="EMAIL"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="PASSWORD"
+        />
         <button type="submit">로그인</button>
       </form>
     </>
